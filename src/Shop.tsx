23af393737f620516e3177cd97892df50052dc73
aziaziazi <aziaziazi@gmail.com>
2025-02-24 02:37:30 +0100

    update styles

--- src/Shop.tsx
+++ src/Shop.tsx
@@ -1,28 +1,39 @@
 import * as React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import {TCategory, TProduct, TSupermarket, categories, defaultProduct} from './data'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import './Shop.css'
 
 import Product from './Product'
 
 export default function Shop({onClose, shop}: {shop: TSupermarket, onClose: any}) {
-  const [currentCategory, setCurrentCategory] = React.useState<TCategory>(() => categories[0])
+  const defaultCat = categories[0]
+  const [currentCategory, setCurrentCategory] = React.useState<TCategory>(defaultCat)
   const selectedProducts = shop.products.filter(p => p.category === currentCategory)
 
   return (
     <div className='shop_container'>
       <div className='shop_info'>
         <div className='shop_header'>
-          <button onClick={onClose}>X</button>
-          <div style={{marginLeft: 10}}>
-            <h2 style={{margin: 0}}>{shop.name}</h2>
-            <i>{shop.address}</i>
+            <i onClick={onClose} className="shop_backButton bi bi-caret-left"></i>
+          <div>
+            <h3  className='shop_name'>{shop.name}</h3>
+            <img className='shop_logo' src={shop.logo}/>
+            <a
+              className='shop_address'
+              href={`https://www.google.com/maps/place/${shop.latitude},${shop.longitude}?q=${shop.name}+${shop.address.replace(' ', '+')}`}
+              target='_blank'  
+            >
+              {shop.address}
+            </a>
           </div>
         </div>
         <Nav
+            defaultActiveKey={defaultCat}
             onSelect={(selectedKey: TCategory) => setCurrentCategory(selectedKey)}
             variant="underline"
+            className='categoriesNav'
         >
           {categories.map(category => (
             <Nav.Item key={category}>
@@ -32,6 +43,10 @@ export default function Shop({onClose, shop}: {shop: TSupermarket, onClose: any}
               >{category}</Nav.Link>
             </Nav.Item>
           ))}
+          {/* fake search bar */}
+          {/* <div className='categoriesNav_search'>
+            <i className="bi bi-search"></i>
+          </div> */}
         </Nav>
       </div>
       <div className='products_list'>
